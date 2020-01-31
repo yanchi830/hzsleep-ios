e0e8a77338a5c15fbea146d9684e612747de758d
Chi Yan <chi.yan@domain.com.au>
2020-01-31 17:36:28 +1100

    init project

--- hzsleep/ViewController.swift
+++ hzsleep/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    private var player:AVQueuePlayer?
+    private var playerLayer: AVPlayerLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        super.view.backgroundColor = UIColor.lightGray
+        let mediaPath = URL(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "mp3")!)
+
+        startMedia(mediaUrl: mediaPath)
     }
 
-
+    public func startMedia(mediaUrl: URL) {
+        player = AVQueuePlayer()
+        playerLayer = AVPlayerLayer(player: player)
+        
+        guard let playerLayer = playerLayer else { fatalError("Error creating player layer") }
+        playerLayer.frame = view.layer.bounds
+        view.layer.addSublayer(playerLayer)
+        
+        let mediaAsset = AVURLAsset(url: mediaUrl)
+        mediaAsset.loadValuesAsynchronously(forKeys: ["duration", "playable"]) {
+            DispatchQueue.main.async {
+                let loopItem = AVPlayerItem(asset: mediaAsset)
+                self.player?.insert(loopItem, after: nil)
+                
+                self.player?.play()
+            }
+        }
+    }
 }
-
