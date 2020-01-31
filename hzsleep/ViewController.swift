3943d6ad074ae886e307557c1b65016bc8972241
Chi Yan <chi.yan@domain.com.au>
2020-01-31 16:41:31 +1100

    Initial Commit

---
+++ hzsleep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hzsleep
+//
+//  Created by Chi Yan on 1/31/20.
+//  Copyright © 2020 Chi Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
